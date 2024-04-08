d15e3803a3cf67cba441ffea1a52f28b9e0e36c8
Nafim Ershad <inan.nafim1089@outlook.com>
2024-04-08 20:50:07 +0600

    Started making the contact page

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import KUTE from "kute.js";
+
 import appStore from "./utils/stateManager";
 
 import renderTechComponent from "./components/tech.component";
@@ -5,10 +7,28 @@ import renderToolsComponent from "./components/tools.component";
 
 import "./scss/main.scss";
 
+const app = document.querySelector("#app");
+
 const techTab = document.querySelector<HTMLSpanElement>(".tab.tech");
 const toolsTab = document.querySelector<HTMLSpanElement>(".tab.tools");
 
 const state = appStore.getState();
+
+
+// Wave Animation
+const tween = new KUTE.fromTo(
+    "#wave1", {
+        path: "#wave1",
+    }, {
+        path: "#wave2",
+    }, {
+        repeat: 999,
+        duration: 7000,
+        yoyo: true,
+    }
+);
+
+tween.start();
 
 
 if(screen.width < 1360){
@@ -30,6 +50,7 @@ toolsTab?.addEventListener("click", function(event){
     activateTab("tools");
     renderToolsComponent();
 });
+
 
 function activateTab(tabName: String): void{
     if(tabName === "tech"){
