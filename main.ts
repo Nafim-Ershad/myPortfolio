44809db17088a21ab4c55a7f6bf762425daca726
Nafim Ershad <inan.nafim1089@outlook.com>
2024-06-15 23:45:49 +0600

    Added type=text/javascript in all html to make particlesJS work on main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import KUTE from "kute.js";
 import AOS from "aos";
 
+// import { tsParticles } from "tsparticles";
+
 import "./src/scss/main.scss";
 
 
@@ -28,9 +30,10 @@ tween.start();
 // }
 
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+// // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// let vh = window.innerHeight * 0.01;
+// // Then we set the value in the --vh custom property to the root of the document
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-particlesJS.load('particles', './scripts/particlejs-bg-config.json')
+
+particlesJS.load('particles', './scripts/particlejs-bg-config.json');
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {resolve} from 'path';
 import { defineConfig } from 'vite';
  
 export default defineConfig({
+    base: '/',
     server: {
         host: 'localhost',
         port: 8000
