a43c617dffe40898ee077fa10def179b38ed2281
Nafim Ershad <inan.nafim1089@outlook.com>
2026-01-12 23:47:56 +0600

    Removed unused imports. Also ignored the legacy files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router';
 
 import { AppProvider } from './context/AppContext';
